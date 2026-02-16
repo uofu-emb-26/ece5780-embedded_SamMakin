e3fe449d94f9b729f8c78bc292e3e7d2ae8d2dae
Sam Makin <u1280390@umail.edu>
2026-02-16 11:02:44 -0700

    Lab 3 3.2: set up PWM

--- Src/lab3.c
+++ Src/lab3.c
@@ -8,6 +8,7 @@ void lab3_main(void)
     // clock enables
     __HAL_RCC_GPIOC_CLK_ENABLE();
     __HAL_RCC_TIM2_CLK_ENABLE();
+    __HAL_RCC_TIM3_CLK_ENABLE();
 
     // Pin defs
     GPIO_InitTypeDef gpio = {0};
@@ -21,17 +22,46 @@ void lab3_main(void)
     HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET);
     HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_RESET);
 
-    TIM2->PSC = 7999; // 1 ms tick
-    TIM2->ARR = 250; // 250 ms reset
+    //3.1
+    TIM2->PSC = 7999;
+    TIM2->ARR = 250;
 
-    TIM2->DIER |= TIM_DIER_UIE; // Update event interupt
+    TIM2->DIER |= TIM_DIER_UIE;
 
-    TIM2->EGR |= TIM_EGR_UG;    // load PSC/ARR
-    TIM2->SR  &= ~TIM_SR_UIF;   // clear pending update flag again
+    TIM2->EGR |= TIM_EGR_UG;
+    TIM2->SR  &= ~TIM_SR_UIF;
 
-    NVIC_EnableIRQ(TIM2_IRQn);// wire interupt to CPU
+    NVIC_EnableIRQ(TIM2_IRQn);
 
-    TIM2->CR1 |= TIM_CR1_CEN;   // start timer
+    TIM2->CR1 |= TIM_CR1_CEN;
+
+
+    //3.2
+    TIM3->PSC = 9;
+    TIM3->ARR = 1000;
+
+    TIM3->EGR |= TIM_EGR_UG; //update
+    TIM3->SR  &= ~TIM_SR_UIF; //clear flag  
+
+   
+    TIM3->CCMR1 = 0; //starting bit
+
+    TIM3->CCMR1 |= (0 << TIM_CCMR1_CC1S_Pos); // enCH1
+    TIM3->CCMR1 |= (7 << TIM_CCMR1_OC1M_Pos); // pwm mode 2
+    TIM3->CCMR1 |= TIM_CCMR1_OC1PE;
+
+    TIM3->CCMR1 |= (0 << TIM_CCMR1_CC2S_Pos);// enCH2
+    TIM3->CCMR1 |= (6 << TIM_CCMR1_OC2M_Pos);
+    TIM3->CCMR1 |= TIM_CCMR1_OC2PE;
+
+    //connect pins
+    TIM3->CCER |= TIM_CCER_CC1E;
+    TIM3->CCER |= TIM_CCER_CC2E;
+
+    TIM3->CCR1 = 200;
+    TIM3->CCR2 = 200;
+
+    TIM3->CR1 |= TIM_CR1_CEN;
 }
 
 
@@ -40,7 +70,7 @@ void TIM2_IRQHandler(void)
 {
     if (TIM2->SR & TIM_SR_UIF) {
 
-        TIM2->SR &= ~TIM_SR_UIF;     //clear flag
+        TIM2->SR &= ~TIM_SR_UIF;
 
         if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_8) == GPIO_PIN_SET) {
             HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_RESET);
