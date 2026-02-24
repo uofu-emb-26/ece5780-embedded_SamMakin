a20990ff46868a497100aeac9d0cdc826eec214f
Sam Makin <u1280390@umail.edu>
2026-02-23 21:18:15 -0700

    lab4: init USART3 @115200 (no TX yet)

--- Src/lab4.c
+++ Src/lab4.c
@@ -1,6 +1,8 @@
 #include <stm32f0xx_hal.h>
 #include <assert.h>
 
+#define UART_BAUD 115200u
+
 static void gpio_usart3_init(void)
 {
     __HAL_RCC_GPIOB_CLK_ENABLE();
@@ -12,10 +14,22 @@ static void gpio_usart3_init(void)
     GPIOB->AFR[1] |=  ((4u   << ((10 - 8) * 4)) | (4u   << ((11 - 8) * 4)));
 }
 
+static void usart3_init(void)
+{
+    __HAL_RCC_USART3_CLK_ENABLE();
+
+    USART3->CR1 = 0;
+    USART3->BRR = (uint16_t)(HAL_RCC_GetHCLKFreq() / UART_BAUD);
+    USART3->CR1 |= USART_CR1_TE | USART_CR1_RE;
+    USART3->CR1 |= USART_CR1_UE;
+}
+
 void lab4_main(void)
 {
     HAL_Init();
+
     gpio_usart3_init();
+    usart3_init();
 
     while (1) { }
 }
