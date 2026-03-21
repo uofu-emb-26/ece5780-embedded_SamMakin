e45fbb6eea46b2dcbcc389f3986b3d4ef3b82930
Sam Makin <u1280390@umail.edu>
2026-03-21 12:38:24 -0600

    fix: remove nested repo, track files normally

--- Src/lab6.c
+++ Src/lab6.c
@@ -1,11 +1,24 @@
 #include "stm32f0xx.h"
 #include <stdint.h>
 
+static void delay_ms(uint32_t t);
+
 static void leds_init(void);
 static void leds_off(void);
 static void show_value_on_leds(uint8_t value);
+
 static void adc_pin_init(void);
 static void adc_init(void);
+
+static void dac_pin_init(void);
+static void dac_init(void);
+
+static const uint8_t sine_table[32] = {
+    127,151,175,197,216,232,244,251,
+    254,251,244,232,216,197,175,151,
+    127,102,78,56,37,21,9,2,
+    0,2,9,21,37,56,78,102
+};
 
 int lab6_main(void)
 {
@@ -13,13 +26,39 @@ int lab6_main(void)
     SystemClock_Config();
 
     leds_init();
+
     adc_pin_init();
     adc_init();
+
+    dac_pin_init();
+    dac_init();
+
+    uint32_t index = 0;
 
     while (1)
     {
         uint8_t adc_value = (uint8_t)(ADC1->DR & 0xFFu);
         show_value_on_leds(adc_value);
+
+        DAC->DHR8R1 = sine_table[index];
+
+        index++;
+        if (index >= 32)
+        {
+            index = 0;
+        }
+
+        delay_ms(1);
+    }
+}
+
+static void delay_ms(uint32_t t)
+{
+    for (uint32_t j = 0; j < t; j++)
+    {
+        for (volatile uint32_t i = 0; i < 8000; i++)
+        {
+        }
     }
 }
 
@@ -98,4 +137,20 @@ static void adc_init(void)
     }
 
     ADC1->CR |= ADC_CR_ADSTART;
+}
+
+static void dac_pin_init(void)
+{
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+
+    GPIOA->MODER |= (3u << (4 * 2));
+    GPIOA->PUPDR &= ~(3u << (4 * 2));
+}
+
+static void dac_init(void)
+{
+    RCC->APB1ENR |= RCC_APB1ENR_DACEN;
+
+    DAC->CR &= ~(DAC_CR_TEN1 | DAC_CR_TSEL1);
+    DAC->CR |= DAC_CR_EN1;
 }
